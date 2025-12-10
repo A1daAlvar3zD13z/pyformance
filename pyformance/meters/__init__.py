3a79c1e827dd2e7fdd70224174ce3755159cc78d
A1daAlvar3zD13z <aida.alvarez@logicmelt.com>
2025-12-10 09:59:00 +0100

    Updated meters folder

--- pyformance/meters/__init__.py
+++ pyformance/meters/__init__.py
@@ -23,4 +23,4 @@ from .timer import Timer
 
 __all__ = ["Counter", "CallbackGauge", "Gauge", "SimpleGauge", "Histogram", "Meter", "Timer"]
 
-any_meter = Histogram | Meter | Gauge[int | float] | Timer | Counter
+any_meter = Histogram | Meter | Gauge | Timer | Counter
